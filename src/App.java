79b71df259e51363b2f07b9a15d2ffe358ef7d50
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-03-04 22:16:44 -0300

    feat: added variables

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App { // classe 
+    public static void main (String [] args) {//metodo
+        final String br = "Brasil"; /*Nunca será alterada */
+        int idade = 12;
+        float altura = 1.60f;
+    
+    }
+}
