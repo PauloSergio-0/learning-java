9dca3d4f3617240496be44e8b349bdd891652a61
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-03-12 17:25:07 -0300

    feat: finished basic JAVA

--- src/edu/paulo/sextasemana/cepexception/FomatarCep.java
+++ src/edu/paulo/sextasemana/cepexception/FomatarCep.java
@@ -2,10 +2,19 @@ package edu.paulo.sextasemana.cepexception;
 
 public class FomatarCep {
     public static void main(String[] args) {
-
+        try {
+            String cepFormat = fomatarCeps("49101258");
+            System.out.println(cepFormat);
+        } catch (CepInvalidoException e) {
+            System.out.println("Cep não corresponde com a regra de negocio");
+        }
     }
-    static  String fomatarCep(String cep) throws CepInvalidoException{
 
+    static String fomatarCeps(String cep) throws CepInvalidoException{
+        if(cep.length() != 8){
+            throw  new CepInvalidoException();
+        }
+            return "49.101-268";
     }
 
 }
